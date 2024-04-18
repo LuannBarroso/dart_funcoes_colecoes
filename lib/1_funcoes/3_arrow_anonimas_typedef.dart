c41f652b62d29d52c72a0e6016456b50dbb1b1ae
LuanBarroso <luan23.lba@gmail.com>
2024-04-18 14:31:02 -0400

    feat: arros anonymous

---
+++ lib/1_funcoes/3_arrow_anonimas_typedef.dart
@@ -0,0 +1,9 @@
+void main() {
+  print(somaDosInteiros(10, 10));
+}
+
+// 3 Partes
+// 1 Pipo de retorno
+// 2 Nome
+// 3 Parametros (normais, nomeados, opcionais)
+int somaDosInteiros(int numero1, int numero2) => numero1 + numero2;
