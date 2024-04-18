6113b6596d36125e81572b1ae0fa9fa79bbb261a
LuanBarroso <luan23.lba@gmail.com>
2024-04-18 18:18:01 -0400

    editing arrow

--- lib/1_funcoes/3_arrow_anonimas_typedef.dart
+++ lib/1_funcoes/3_arrow_anonimas_typedef.dart
@@ -1,5 +1,18 @@
 void main() {
+  // Funcoes Arrow - ok
+  // Funcoes Anonimas - ok
+  // TypeDef
   print(somaDosInteiros(10, 10));
+
+  var nome = 'macaquito';
+  var idade = 13;
+  //Função anonima
+  var funcaoQualquer = () {
+    print('chamou um macaquito de macaquito');
+    return '2132134135';
+  };
+
+  print(funcaoQualquer());
 }
 
 // 3 Partes
@@ -7,3 +20,12 @@ void main() {
 // 2 Nome
 // 3 Parametros (normais, nomeados, opcionais)
 int somaDosInteiros(int numero1, int numero2) => numero1 + numero2;
+
+void chamarUmaFuncaoDeUmParametro(FuncaoQueRecebeNome funcaoQueRecebeNome) {
+  var calculo = 1 + 1;
+  var nomeCompleto = 'Luan Barroso';
+  print('finalizando a funcao chamarUmaFuncaoDeUmParametro');
+  print('invocando funcaoQueRecebeONome');
+}
+
+typedef FuncaoQueRecebeNome = void Function(String nome);
