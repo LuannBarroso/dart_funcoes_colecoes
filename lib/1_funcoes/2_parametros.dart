14be51881c466a3ef634777c24b711a1d41fcdbc
LuanBarroso <luan23.lba@gmail.com>
2024-04-17 20:11:52 -0400

    to add new parameters

--- lib/1_funcoes/2_parametros.dart
+++ lib/1_funcoes/2_parametros.dart
@@ -1,3 +1,9 @@
 void main() {
-  //
+  // Parametros podem ser promovidos a non null se forem checados.
+  // Parametros Nomeados são nullables por default
+  print('A soma de 10 + 10 é ${somaInteiros(10, 10)}');
+}
+
+int somaInteiros(int numero1, int numero2) {
+  return numero1 + numero2;
 }
