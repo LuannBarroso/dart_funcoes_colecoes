b7f24c3d819d8c1d509464bdf17c957ffa78bcf5
LuanBarroso <luan23.lba@gmail.com>
2024-04-22 18:30:13 -0400

    created new type of list using sort

--- lib/2_listas/1_listas.dart
+++ lib/2_listas/1_listas.dart
@@ -31,4 +31,37 @@ void main() {
   } else {
     print('Nem todos os nomes tem a letra J');
   }
+
+  // sort
+
+  print('.sort');
+  var listaParaOrdernacao = [99, 22, 33, 55, 54, 477, 132132];
+
+  listaParaOrdernacao.sort();
+  print(listaParaOrdernacao);
+
+  var listaPacientes = [
+    'Luan Barroso |29',
+    'Luana Barroso |35',
+    'Guilherme |18',
+    'Pedro Virtosa |15',
+    'Antonio Madeira|48',
+  ];
+
+  listaPacientes.sort((paciente1, paciente2) {
+    final pacienteDados1 = paciente1.split('|');
+    final pacienteDados2 = paciente2.split('|');
+
+    final idadePaciente1 = int.parse(pacienteDados1[1]);
+    final idadePaciente2 = int.parse(pacienteDados2[1]);
+
+    if (idadePaciente1 > idadePaciente2) {
+      return 1;
+    } else if (idadePaciente1 == idadePaciente2) {
+      return 0;
+    } else {
+      return -1;
+    }
+  });
+  print(listaPacientes);
 }
