062b5505c5da4d163ffc72bfac89475007a739cb
LuanBarroso <luan23.lba@gmail.com>
2024-04-26 22:57:07 -0400

    editing exceptions

--- lib/6_exceptions/1_exceptions.dart
+++ lib/6_exceptions/1_exceptions.dart
@@ -1,10 +1,17 @@
 // ignore: file_names
 void main() {
   var idade = '38 anos';
-
+  String? nome;
   try {
     int.parse(idade);
-  } catch (erro) {
-    print(erro);
+    nome!.toLowerCase();
+  } on FormatException catch (e, s) {
+    print(s);
+  } on TypeError catch (e) {
+    print('Erro ao converter idade');
+  } catch (e) {
+    print('Erro ao execultar o programa');
+  } finally {
+    print('finally');
   }
 }
