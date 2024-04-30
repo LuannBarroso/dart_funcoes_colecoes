54b52cded7160aa92f08f72f318c66f4e3c6ad40
LuanBarroso <luan23.lba@gmail.com>
2024-04-29 21:13:01 -0400

    end of sum

--- lib/7_imports/1_imports.dart
+++ lib/7_imports/1_imports.dart
@@ -1,3 +1,10 @@
 import 'somas/soma.dart';
 
-void main() {}
+void main() {
+  var totalDoubles = somaDoubles(10.1, 5.1);
+
+  var totalInteiros = somaInteiros(12, 14);
+
+  print('Total de doubles: $totalDoubles');
+  print('Total de inteiros: $totalInteiros');
+}
