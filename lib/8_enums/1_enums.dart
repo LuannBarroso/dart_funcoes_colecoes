3280b65062f1c631a4f4a6fb2b372f91705b97d6
LuanBarroso <luan23.lba@gmail.com>
2024-04-30 13:00:49 -0400

    new way to print enums

--- lib/8_enums/1_enums.dart
+++ lib/8_enums/1_enums.dart
@@ -1,8 +1,16 @@
 void main() {
   var cor = Cores.vermelho;
 
-  if (cor == Cores.laranja) {}
-  print('A cor é laranja $Cores.laranja');
+  if (cor == Cores.vermelho) {}
+
+  //Transformando a string azul em um enum COres.azul.
+  // Antes da versão 2.15
+  var azul = 'azul';
+  var corAzul = Cores.values.where((cor) => cor.toString() == 'Cores.$azul');
+  print(corAzul);
+
+  //A parti da versão 2.15
+  print(Cores.azul.name);
 }
 
 enum Cores { azul, vermelho, laranja, verde, preto }
